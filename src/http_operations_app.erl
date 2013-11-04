7b1d0708318812ab98c9f157fdba999259278224
markdev <sukaloh4mo>
2013-11-04 17:11:39 +0400

    fixfix

--- src/http_operations_app.erl
+++ src/http_operations_app.erl
@@ -1,3 +1,4 @@
+
 %% Feel free to use, reuse and abuse the code in this file.
 
 %% @private
@@ -11,21 +12,19 @@
 %% API.
 
 start(_Type, _Args) ->
-	Dispatch = cowboy_router:compile([
-		{'_', [
-			{"/", http_operations_toppage_handler, []}
-		]}
-	]),
+    Dispatch = cowboy_router:compile([{'_', [
+					     {"/", http_operations_toppage_handler, []}
+					    ]}
+				     ]),
     Priv = code:priv_dir(http_operations),
-	{ok, _} = cowboy:start_https(http, 100, [{port, 8080},
-						 {verify,verify_peer},
-						 {fail_if_no_peer_cert,true},
-						 {cacertfile,Priv ++ "/server/ca.pem"},
-						 {keyfile,Priv ++ "/server/key.pem"},
-						 {certfile,Priv ++ "/server/cert.pem"}], [
-		{env, [{dispatch, Dispatch}]}
-	]),
-	http_operations_sup:start_link().
+    SSLOpts = [{verify,verify_peer},
+	       {fail_if_no_peer_cert,true},
+	       {cacertfile,filename:join([Priv,"server","ca.pem"])},
+	       {keyfile,filename:join([Priv,"server2","key.pem"])},
+	       {certfile,filename:join([Priv,"server2","cert.pem"])}],
+    {ok, _} = cowboy:start_https(http, 100, [{port, 8080} | SSLOpts],
+				 [{env, [{dispatch, Dispatch}]}]),
+    http_operations_sup:start_link().
 
 stop(_State) ->
 	ok.
